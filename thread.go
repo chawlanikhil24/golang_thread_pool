cc6735f9bcc02019c49c29630f962e34fb7933a4
Nikhil Chawla <chawlanikhil24@gmail.com>
2022-02-27 01:25:19 +0530

    support import fixes

--- thread.go
+++ thread.go
@@ -12,8 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-
-package thread_pool
+package golang_thread_pool
 
 // ----------------------------------------------------------------
 type ThreadInfo struct {
--- util.go
+++ util.go
@@ -12,7 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-package thread_pool
+package golang_thread_pool
 
 import (
 	"math/rand"
